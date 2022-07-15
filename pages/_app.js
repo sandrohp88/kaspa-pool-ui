bd4d74ae26e1902ff0285a3f97540fc8b68c3049
Sandro Herrera Pallares <sandrohp88@gmail.com>
2022-07-15 14:55:30 -0700

    Blank template

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import PropTypes from "prop-types";
+import "../styles/globals.css";
+import Head from "next/head";
+
+const propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired, // eslint-disable-line
+};
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />;
+      <Head>
+        <title>Nerd Pool</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+    </>
+  );
 }
-
-export default MyApp
+MyApp.propTypes = propTypes;
+export default MyApp;
