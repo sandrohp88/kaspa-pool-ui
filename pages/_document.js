2f15f8813b26b6e0d04ee54f6ac612bff6e4da35
Sandro Herrera Pallares <sandrohp88@gmail.com>
2022-07-18 13:11:02 -0700

    Add global styles through _document

---
+++ pages/_document.js
@@ -0,0 +1,13 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en" className="bg-neutral-800 text-neutral-300 ">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
